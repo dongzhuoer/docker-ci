c693ad60ec4600b6c0bbb76e6047e9f248f6f6d1
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2019-12-25 09:54:16 +0800

    u

--- base.Dockerfile
+++ base.Dockerfile
@@ -2,5 +2,5 @@ FROM dongzhuoer/ubuntu-cn:eoan
 
 LABEL maintainer="Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>"
 
-RUN apt update 
+RUN apt update
 RUN apt -y install git wget pandoc
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-tag=$1
-
-docker build -t dongzhuoer/ci:$tag -f $tag.Dockerfile .
-
 echo "$DOCKER_PASSWORD" | docker login -u dongzhuoer --password-stdin
 docker push dongzhuoer/ci:$tag
